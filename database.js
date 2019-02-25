4bd10a6db579414cb09c168b9a1bc17c16eaa6e8
Steffen Hofbauer <steffen.hofbauer@googlemail.com>
2019-02-25 09:12:13 +0100

    database

---
+++ database.js
@@ -0,0 +1,52 @@
+var Connection = require('tedious').Connection;
+var Request = require('tedious').Request;
+
+// Create connection to database
+var config =
+{
+    userName: 'steffen', // update me
+    password: 'F646bb91#', // update me
+    server: 'autokaufempfehlung.database.windows.net', // update me
+    options:
+    {
+        database: 'Autokaufempfehlung', //update me
+        encrypt: true
+    }
+}
+var connection = new Connection(config);
+
+// Attempt to connect and execute queries if connection goes through
+connection.on('connect', function(err)
+    {
+        if (err)
+        {
+            console.log(err)
+        }
+        else
+        {
+            queryDatabase()
+        }
+    }
+);
+
+function queryDatabase()
+{
+    console.log('Reading rows from the Table...');
+
+    // Read all rows from table
+    var request = new Request(
+        "SELECT * FROM audi",
+        function(err, rowCount, rows)
+        {
+            console.log(rowCount + ' row(s) returned');
+            process.exit();
+        }
+    );
+
+    request.on('row', function(columns) {
+        columns.forEach(function(column) {
+            console.log("%s\t%s", column.metadata.colName, column.value);
+        });
+    });
+    connection.execSql(request);
+}
